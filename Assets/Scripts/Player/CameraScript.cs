ae1e21e23a48242cf4638b5e97d54c989f6fe9c6
JoelNamaZuzzi <59820667+JoelNamaZuzzi@users.noreply.github.com>
2020-09-29 11:59:22 +0200

    No more collider, more trigger

---
+++ Assets/Scripts/Player/CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform Target;
+    
+    [SerializeField]
+    private float distance = -10f;
+    void Update()
+    {
+        var transformPositionx = transform.position;
+        transformPositionx.x = Target.position.x-distance;
+
+        var transformPositiony = transform.position;
+        transformPositiony.y = Target.position.y+4;
+
+        var transformPositionz = transform.position;
+        transformPositionz.z = Target.position.z;
+        
+        transform.position = new Vector3(transformPositionx.x, transformPositiony.y, transformPositionz.z);
+
+    }
+}
--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -14,12 +14,34 @@ public class PlayerScript : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right*z+transform.forward *x;
+
+        Vector3 move = (transform.right * z + transform.forward * x);
 
         Controller.Move(move*speed*Time.deltaTime);
+        
+        /*if (x <= 0.99f)
+        {
+            transform.localRotation = Quaternion.Euler(0,90,0);
+            
+            
+        }else if (x >= -0.99f && x<= 0.1f)
+        {
+            transform.localRotation = Quaternion.Euler(0,-90,0);
+        }
+
+        if (z <= 0.99f)
+        {
+            transform.localRotation = Quaternion.Euler(0,0,0);
+            
+        }else if (z >= -0.99f && z <= 0.1f)
+        {
+            transform.localRotation = Quaternion.Euler(0,180,0);
+        }*/
+
+        
     }
     
-    private void OnCollisionEnter(Collision other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Grass"))
         {
