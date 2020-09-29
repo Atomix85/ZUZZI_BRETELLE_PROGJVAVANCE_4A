1ec33fb8192b1be38849fe704fc2979abef4d766
JoelNamaZuzzi <59820667+JoelNamaZuzzi@users.noreply.github.com>
2020-09-29 09:33:13 +0200

    Created Tags, rigidbody & collider on prefab, TestScene and basic player script
    Need to fix collision w\ grass and add Smooth Camera follow

---
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController Controller;
+    [SerializeField]
+    private float speed = 12f;
+    
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right*z+transform.forward *x;
+
+        Controller.Move(move*speed*Time.deltaTime);
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Grass"))
+        {
+            Debug.Log("Grass Col");
+        }
+        else if (other.gameObject.layer == LayerMask.NameToLayer("Bridge"))
+        {
+            Debug.Log("Col Bridge");
+        }
+    }
+}
